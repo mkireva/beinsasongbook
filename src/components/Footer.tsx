437121f282cec94bb73124177fd98f40a397f348
mkireva <mariya@kireva.de>
2024-05-24 12:16:45 +0200

    fix: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,24 @@
 export default function Footer() {
   return (
-    <footer className="w-full bg-gray-400">
+    <footer className="w-full bg-gray-500">
       <div className="w-full p-10 mx-auto max-w-screen-xl md:flex md:items-center md:justify-between sm:flex sm:items-center sm:justify-between">
-        <span className=" text-white sm:text-center text-xl font-semibold">
-          
-          <a href="https://www.kireva.de/" className="hover:underline gap-4 text-xl font-semibold mr-2 ">
-          2024
+        <span className=" text-white text-xl font-semibold">
+          <a
+            href="https://www.kireva.de/"
+            className="hover:underline gap-4 text-xl font-semibold mr-2 "
+          >
+            2024
           </a>
-           Мария Кирева
+          Мария Кирева
         </span>
-        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-white sm:mt-0">
+        <ul className="flex flex-wrap mt-3 text-sm font-medium text-white sm:mt-0">
           <li>
-            <a target="_blank" rel="noopener noreferrer" href="https://www.kireva.de/" className="hover:underline text-xl font-semibold">
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.kireva.de/"
+              className="hover:underline text-xl font-semibold"
+            >
               Контакт
             </a>
           </li>
