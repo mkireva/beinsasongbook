3415ffd15c3f702a4feb561197e62315c3c53ab2
mkireva <mariya@kireva.de>
2024-05-31 17:16:19 +0200

    fix: description

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,13 @@ function App() {
   return (
     <div className="flex flex-col min-h-screen items-center justify-center">
       <div className="container">
-      <h2 className="text-2xl text-center font-semibold  text-sky-700 mt-20">
-        Петър Дънов
-      </h2>
-      <h1 className="text-5xl text-center font-bold  text-sky-700 mt-6">
-        Песни от Учителя
-      </h1>
-      {/* <h1 className="text-xl p-3 text-center text-sky-700 mt-3">
+        <h2 className="text-2xl text-center font-semibold  text-sky-700 mt-20">
+          Петър Дънов
+        </h2>
+        <h1 className="text-5xl text-center font-bold  text-sky-700 mt-6">
+          Песни от Учителя
+        </h1>
+        {/* <h1 className="text-xl p-3 text-center text-sky-700 mt-3">
         Издадени от <br />
         <span className="space-x-2">
           <span> Петър Ганев</span>
@@ -21,57 +21,57 @@ function App() {
           <span> Мария Кирева</span>
         </span>
       </h1> */}
-      <h1 className="text-xl p-3 italic text-center text-sky-700 mt-3">
-        Четвърто преработено и допълнено издание
-      </h1>
-      <h1 className="text-xl text-center font-semibold text-sky-700 mt-3">
-        2024
-      </h1>
-      <div className="max-w-5xl grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-2 mx-auto my-10 pl-9 pr-9 pt-7 pb-8 bg-sky-100 rounded-lg mb-20">
-        <div className="my-6">
-          <img src={image} alt="" className="w-auto shadow-xl rounded-xl" />
-        </div>
-        <div className=" my-4 max-w-4xl mx-auto p-2">
-          <p className=" ml-10 text-xl text-start font-semibold  text-sky-700">
-            Изданието съдържа вокални и инструментални композиции от
-            Учителя Петър Дънов и негови ученици.
-          </p>
-          <p className=" mt-5 ml-10 text-xl text-start font-semibold text-sky-700">
-            Интерактивният пдф-документ предлага:
-          </p>
-          <ul className="mt-3 pl-5 pr-5 list-disc">
-            <li className="mt-1 ml-10 text-xl font-semibold text-sky-700">
-              Азбучно съдържание за мобилни устройства с навигация към всяка
-              песен
-            </li>
-            <li className="ml-10 text-xl font-semibold  text-sky-700">
-              Навигация от съдържанието на документа
-            </li>
-            <li className="ml-10 text-xl font-semibold  text-sky-700">
-              Навигация от заглавието на всяка песен до съответната обяснителна
-              бележка
-            </li>
-            <li className="ml-10 text-xl font-semibold  text-sky-700">
-              Обратна навигация от обяснителната бележка на песента към
-              съответната песен
-            </li>
-            <li className="ml-10 text-xl font-semibold  text-sky-700">
-              Азбучен показател с навигация към съответната песен
-            </li>
-          </ul>
-          <div className=" my-10 flex items-center justify-center">
-            <button className="bg-sky-600 w-3/4 py-2 rounded-md items-center justify-center shadow-2xl">
-              <a
-                href={songbook}
-                download="Песни от Учителя 2024"
-                className="text-white text-2xl font-semibold"
-              >
-                Download
-              </a>
-            </button>
+        <h1 className="text-xl p-3 italic text-center text-sky-700 mt-3">
+          Четвърто преработено и допълнено издание
+        </h1>
+        <h1 className="text-xl text-center font-semibold text-sky-700 mt-3">
+          2024
+        </h1>
+        <div className="max-w-5xl grid lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 gap-2 mx-auto my-10 pl-9 pr-9 pt-7 pb-8 bg-sky-100 rounded-lg mb-20">
+          <div className="my-6">
+            <img src={image} alt="" className="w-auto shadow-xl rounded-xl" />
+          </div>
+          <div className=" my-4 max-w-4xl mx-auto p-2">
+            <p className=" md:ml-10 text-xl text-start font-semibold  text-sky-700">
+              Изданието съдържа вокални и инструментални композиции от Учителя
+              Петър Дънов и негови ученици.
+            </p>
+            <p className=" mt-5 md:ml-10 text-xl text-start font-semibold text-sky-700">
+              Интерактивният пдф-документ предлага:
+            </p>
+            <ul className="mt-3 pl-5 pr-5 list-disc">
+              <li className="mt-1 md:ml-10 text-xl font-semibold text-sky-700">
+                Азбучно съдържание за мобилни устройства с навигация към всяка
+                песен
+              </li>
+              <li className="md:ml-10 text-xl font-semibold  text-sky-700">
+                Навигация от съдържанието на документа
+              </li>
+              <li className="md:ml-10 text-xl font-semibold  text-sky-700">
+                Навигация от заглавието на всяка песен до съответната
+                обяснителна бележка
+              </li>
+              <li className="md:ml-10 text-xl font-semibold  text-sky-700">
+                Обратна навигация от обяснителната бележка на песента към
+                съответната песен
+              </li>
+              <li className="md:ml-10 text-xl font-semibold  text-sky-700">
+                Азбучен показател с навигация към съответната песен
+              </li>
+            </ul>
+            <div className=" my-10 flex items-center justify-center">
+              <button className="bg-sky-600 w-3/4 py-2 rounded-md items-center justify-center shadow-2xl">
+                <a
+                  href={songbook}
+                  download="Песни от Учителя 2024"
+                  className="text-white text-2xl font-semibold"
+                >
+                  Download
+                </a>
+              </button>
+            </div>
           </div>
         </div>
-      </div>
       </div>
       <Footer />
     </div>
